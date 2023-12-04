1aca9159edb7a851696b595b860cb92309eaadef
ack909 <92495276+ack909@users.noreply.github.com>
2023-12-04 12:35:46 -0800

    Update Shop.jsx
    Frag1 img

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import f1 from '../Components/fragrant1.jpeg'
 const Shop = () => {
   return (
-    <div>
+    <div className="frag1">
+      <img src={f1} alt=""/>
+    </div>
     
     </div>
   )
