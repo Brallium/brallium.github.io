7633daec5540e4a70e7da63a7ee179726eb2c45d
ack909 <92495276+ack909@users.noreply.github.com>
2023-12-04 12:45:27 -0800

    Update Shop.jsx
    standard img

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import f1 from '../Components/fragrant1.jpeg'
+import f1 from './Components/fragrant1.jpeg'
+import stand from './Components/Standard ver.jpeg'
 const Shop = () => {
   return (
     <div className="frag1">
       <img src={f1} alt=""/>
     </div>
-    
+    <div className="standard">
+      <img src={stand} alt=""/>
     </div>
+    
   )
 }
 
