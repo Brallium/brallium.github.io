2e5ad9795101e8e0f72d99a1fdf7620626e028f8
MATCHAONM <120128884+MATCHAONM@users.noreply.github.com>
2023-12-05 15:09:24 -0800

    login backend server fixed, but page not shown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react"
-import './styles/App.css';
+import React, { useState } from "react";
+import "./styles/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from './Components/NavBar/Navbar';
-import Shop from './Pages/Shop';
-import contents from './Components/contents';
+import Navbar from "./Components/NavBar/Navbar";
+import Shop from "./Pages/Shop";
+import contents from "./Components/contents";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import { feedback } from "./Components/feedback";
@@ -13,65 +13,42 @@ import FAQ from "./Pages/FAQ";
 import Cart from "./Pages/Cart";
 import Profile from "./Pages/Profile";
 import MainPage from "./Pages/MainPage";
-import { AuthContextProvider } from './config/AuthContext';
+import { AuthContextProvider } from "./config/AuthContext";
 import { Products } from "./Components/products";
 import Paypal from "./Paypal";
 import ShoppingCart from "./Components/ShoppingCart";
-// import "./App.css";
-// import Navbar from "./Components/NavBar/Navbar";
-// import { Products } from "./Components/products";
-// import contents from "./Components/contents";
-// import { Login } from "./Login";
-// import { Register } from "./Register";
-import axios from "axios";
-// import { AuthContextProvider } from "./Config/AuthContext";
 
+function App() {
+  const [currentForm, setCurrentForm] = useState("login");
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
 
-export default function App() {
-  const [currentForm, setCurrentForm] = useState('login')
-
-  axios.defaults.baseURL = "http://localhost:3000";
-  axios.defaults.withCredentials = true;
-
-  function App() {
-    const [currentForm, setCurrentForm] = useState("login");
-    const toggleForm = (formName) => {
-      setCurrentForm(formName);
-    };
-
-    return (
-      <>
-        {/* <AuthContextProvider>
-        <SignInPage />
-      </AuthContextProvider> */}
+  return (
+    <>
+      <AuthContextProvider>
+        {/* <SignInPage /> */}
 
         <div>
           <Router>
             <Navbar />
             <Routes>
-              <Route path='/' element={<MainPage />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/shop' element={<Shop />}>
-                <Route path=':productId' element={<Shop />} />
-
+              <Route path="/" element={<MainPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/shop" element={<Shop />}>
+                <Route path=":productId" element={<Shop />} />
               </Route>
-              <Route path='/cart' element={<ShoppingCart />} />
+              <Route path="/cart" element={<ShoppingCart />} />
               {/* <Route path='/login' element={<LoginSignUp />} /> */}
-              <Route path='/blog' element={<Blogs />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/Profile' element={<Profile />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/faq' element={<FAQ />} />
-
+              <Route path="/blog" element={<Blogs />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/Profile" element={<Profile />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/faq" element={<FAQ />} />
             </Routes>
           </Router>
           {/* <div className='App'>
           {contents.map(contents => (
-    <AuthContextProvider>
-      <div>
-        <Navbar />
-        <div className="App">
-          {contents.map((contents) => (
             <Products
               key={contents.id}
               image={contents.image}
@@ -88,14 +65,9 @@ export default function App() {
         </div>
         <Paypal></Paypal> */}
         </div>
-        {
-          currentForm === "login" ? (
-            <Login onFormSwitch={toggleForm} />
-          ) : (
-            <Register onFormSwitch={toggleForm} />
-          )
-        }
-      </>
-    );
-  }
+      </AuthContextProvider>
+    </>
+  );
 }
+
+export default App;
