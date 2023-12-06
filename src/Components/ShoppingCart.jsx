f93b90b0791305ab45924b341ab3dbc09d9e547c
dinothaurs <janiekuang8679@gmail.com>
2023-12-05 22:58:48 -0800

    styling changes for payment page

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -36,29 +36,43 @@ const Shop = () => {
                 <div className="cart-item-wrap">
                     <img 
                         className='cart-item-image' 
-                        src={"https://nevanude.com/cdn/shop/products/Nevanude62105daf33b4c262105daf33ba8.3534814962105daf33ba8.jpg?v=1660174719"}
+                        src={process.env.PUBLIC_URL + "./images/Standard.jpeg"}
                         alt='item image'
                     />
                     <div className="text-container">
-                        <h className = "item-name">Item Name</h>
-                        <h className = "item-quantity"> Size: A-C </h>
-                        <h className = "item-quantity"> Color: </h>
-                        <h className = "item-quantity"> Quantity:</h>
-                        <h className = "item-price"> $XX.XX </h>
+                        <h className = "item-name">Standard Nipple Cover</h>
+                        <h className = "item-quantity"> Size: A </h>
+                        <h className = "item-quantity"> Color: #55321E </h>
+                        <h className = "item-quantity"> Quantity: 1</h>
+                        <h className = "item-price"> $15.00 </h>
                     </div>
                 </div>
                 <div className="cart-item-wrap">
                     <img 
                         className='cart-item-image' 
-                        src={"https://nevanude.com/cdn/shop/products/Nevanude62105daf33b4c262105daf33ba8.3534814962105daf33ba8.jpg?v=1660174719"}
+                        src={process.env.PUBLIC_URL + "./images/fragrant1.jpeg"}
                         alt='item image'
                     />
                     <div className="text-container">
-                        <h className = "item-name">Item Name</h>
-                        <h className = "item-quantity"> Size: A-C </h>
-                        <h className = "item-quantity"> Color: </h>
-                        <h className = "item-quantity"> Quantity:</h>
-                        <h className = "item-price"> $XX.XX </h>
+                        <h className = "item-name">Fragrant Nipple Cover</h>
+                        <h className = "item-quantity"> Size: B </h>
+                        <h className = "item-quantity"> Color: #7D5645</h>
+                        <h className = "item-quantity"> Quantity: 1</h>
+                        <h className = "item-price"> $20.00 </h>
+                    </div>
+                </div>
+                <div className="cart-item-wrap">
+                    <img 
+                        className='cart-item-image' 
+                        src={process.env.PUBLIC_URL + "./images/fragrant2.jpeg"}
+                        alt='item image'
+                    />
+                    <div className="text-container">
+                        <h className = "item-name">Thickened Nipple Cover</h>
+                        <h className = "item-quantity"> Size: C </h>
+                        <h className = "item-quantity"> Color: #D4B594</h>
+                        <h className = "item-quantity"> Quantity: 1</h>
+                        <h className = "item-price"> $18.00</h>
                     </div>
                 </div>
             </div>
@@ -71,7 +85,7 @@ const Shop = () => {
                 <div className="order-summary-money-container">
                     <h className="order-money-text">TBD</h>
                     <h className="order-money-text">TBD</h>
-                    <h className="order-money-text">$XX.XX</h>
+                    <h className="order-money-text">$53.00</h>
                 </div>
                 <Paypal className="paypal"></Paypal>
             </div>
